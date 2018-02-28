2b792005d0a19d1ac507480889e59afd92cece61
Schneider <36104448+brickpool@users.noreply.github.com>
2018-02-28 21:55:29 +0100

    Add files via upload

--- LOGO.ino
+++ LOGO.ino
@@ -1,8 +1,16 @@
 #include <CustomSoftwareSerial.h>
 #include "LogoPG.h"
 
+#define _MONITOR
+
 const byte rxPin = 2;
 const byte txPin = 3;
+
+unsigned long checkTime;
+const unsigned long checkDelay = 5000; // ms
+
+unsigned long cycleTime;
+const unsigned long cycleDelay = 500; // min > 200ms
 
 // set up the SoftwareSerial object
 CustomSoftwareSerial LogoSerial(rxPin, txPin);
@@ -11,67 +19,87 @@ LogoClient LOGO(&LogoSerial);
 
 void setup() {
   // Init Monitor interface
+#ifdef _MONITOR
   Serial.begin(9600);
   while (!Serial) {
    ; // wait for serial port to connect. Needed for native USB port only
   }
+#endif
 
   // Start the SoftwareSerial Library
   LogoSerial.begin(9600, CSERIAL_8E1);
   // Setup Time, 1s.
   delay(1000); 
+#ifdef _MONITOR
   Serial.println("");
   Serial.println("Cable connected");  
   if (LogoSerial.isListening())
     Serial.println("Softserial is listening !");
+#endif
+
+  checkTime = millis();
+  cycleTime = millis();
 }
 
 void loop() 
 {
-  int Result, Status;
-  
   // Connection
   while (!LOGO.Connected)
   {
     if (!Connect())
       delay(2000);
   }
-  
-  Result = LOGO.GetPlcStatus(&Status);
-  if (Result == 0)
+
+  // Operation Mode
+  if (millis()-checkTime > checkDelay)
   {
-    if (Status == LogoCpuStatusRun)
+    checkTime = millis();
+    int Status;
+    int Result = LOGO.GetPlcStatus(&Status);
+    if (Result == 0)
     {
-      Serial.print("FETCH DATA ... ");
-      Result = LOGO.ReadArea(LogoAreaDB, 1, VM_Q01_08, 2, NULL);
-      if (Result == 0)
+      if (Status != LogoCpuStatusRun)
       {
-        Serial.print("Output 8-1, 16-9, analog input 1: 0b");
-        printBinaryByte(LH.ByteAt(VM_Q01_08));
-        Serial.print(" 0b");
-        printBinaryByte(LH.ByteAt(VM_Q09_16));
-        Serial.print(" ");
-        Serial.print(LH.IntegerAt(VM_AI1_Hi));
-        Serial.println();
+#ifdef _MONITOR
+        Serial.println("STARTING THE PROG");
+#endif
+        LOGO.PlcStart(); 
       }
-      else
-        CheckError(Result);
     }
     else
-    {
-      Serial.println("STARTING THE PROG");
-      LOGO.PlcStart(); 
-    }
+      CheckError(Result);
   }
-  else
-    CheckError(Result);
 
-  delay(1000);  
+  if (millis()-cycleTime > cycleDelay)
+  {
+    cycleTime = millis();
+#ifdef _MONITOR
+    Serial.print(cycleTime);
+    Serial.print("ms ");
+    Serial.print("FETCH DATA ... ");
+#endif
+    int Result = LOGO.ReadArea(LogoAreaDB, 1, VM_I01_08, 990-923, NULL);
+    if (Result == 0)
+    {
+#ifdef _MONITOR
+      Serial.print("Output 8-1, 16-9, analog input 1: 0b");
+      printBinaryByte(LH.ByteAt(VM_Q01_08));
+      Serial.print(" 0b");
+      printBinaryByte(LH.ByteAt(VM_Q09_16));
+      Serial.print(" ");
+      Serial.print(LH.IntegerAt(VM_AI1_Hi));
+      Serial.println();
+#endif
+    }
+    else
+      CheckError(Result);
+  }
 }
 
 bool Connect()
 {
   int Result = LOGO.Connect();
+#ifdef _MONITOR
   Serial.println("Try to connect with LOGO");
   if (Result == 0) 
   {
@@ -83,18 +111,23 @@ bool Connect()
   {
     Serial.println("Connection error!");
   }
+#endif
   return Result == 0;
 }
 
 void CheckError(int ErrNo)
 {
+#ifdef _MONITOR
   Serial.print("Error No. 0x");
   Serial.println(ErrNo, HEX);
+#endif
   
   // Checks if it's a LOGO Error => we need to disconnect
   if (ErrNo & 0x00FF)
   {
+#ifdef _MONITOR
     Serial.println("LOGO ERROR, disconnecting.");
+#endif
     LOGO.Disconnect(); 
   }
 }
@@ -102,8 +135,8 @@ void CheckError(int ErrNo)
 // http://forum.arduino.cc/index.php?topic=246920.0
 void printBinaryByte(byte value)
 {
-    for (byte mask = 0x80; mask; mask >>= 1) 
-    {
-        Serial.print((mask & value) ? '1' : '0');
-    }
+  for (byte mask = 0x80; mask; mask >>= 1) 
+  {
+    Serial.print((mask & value) ? '1' : '0');
+  }
 }
