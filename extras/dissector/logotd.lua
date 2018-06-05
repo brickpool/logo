9ffe7a8542fc9b257e248c16450a3990d7c4353d
brickpool <brickpool@web.de>
2018-06-05 15:26:05 +0200

    v0.2.0

--- extras/dissector/logotd.lua
+++ extras/dissector/logotd.lua
@@ -14,8 +14,9 @@
 --  p_frag.lua      by mj99, https://osqa-ask.Wireshark.org/answer_link/55764/
 --
 -- History:
---  0.1   03-04.06.2018   inital version
---  0.1.1 04.06.2018      bug fixing
+--  0.1   03-04.06.2018	inital version
+--  0.1.1 04.06.2018		bug fixing
+--  0.2 	04.06.2018    INFO colum, command code 0x08
 --
 -------------------------------------------------------------------------------
 
@@ -73,8 +74,8 @@ reset_debug_level()
 
 ---------------------------------------- 
 -- creates the Proto object, but doesn't register it yet
-PROFIBUS = Proto("pbus", "PROFIBUS-DP Telegram")
-LOGOTD = Proto("logotd", "LOGO Text Display Protocol")
+PROFIBUS  = Proto("pbus",   "PROFIBUS-DP Telegram")
+LOGOTD    = Proto("logotd", "LOGO Text Display Protocol")
 
 ----------------------------------------
 -- a table of fields for FDL 
@@ -123,6 +124,8 @@ local PBUS_SD2_HDR_LEN = PBUS_FDL_HDR_LEN + 1 + 1 + 1
 local PBUS_DDLM_HDR_LEN = 1 + 1
 -- size of the FDL trailer (DCS+ED = 2 bytes)
 local PBUS_TRAILER_LEN = 1 + 1
+-- size of the ALI header (01+00+BC+CND = 4 bytes)
+local PBUS_ALI_HDR_LEN = 1 + 1 + 1 + 1
 
 fdl_fields.telegram_number  = ProtoField.uint32("PROFIBUS.number",        "Telegram number")
 fdl_fields.flags            = ProtoField.uint8 ("PROFIBUS.flags",         "Flags",                              base.HEX)
@@ -143,27 +146,27 @@ fdl_fields.ed               = ProtoField.uint8 ("PROFIBUS.ED",            "End D
 -- a table of fields for ALI
 local ali_fields = LOGOTD.fields
 
-local FUNCTION_CODES = {
-  [0x01] = "? (Init Start)",
-  [0x02] = "? (Init Complete)",
-  [0x03] = "? (Keep Alive)",
+local COMMAND_CODES = {
+--  [0x01] = "Init Start",
+--  [0x02] = "Init Complete",
+--  [0x03] = "Diagnosis",
   [0x04] = "Stop",
   [0x05] = "Start",
   [0x08] = "Online Test",
   [0x09] = "Function Key",
   [0x10] = "Clock",
-  [0x14] = "?",
-  [0x18] = "? (Clear Screen)",
-  [0x3c] = "?",
-  [0x3d] = "?",
-  [0x40] = "?",
-  [0x5a] = "?",
-  [0x5b] = "?",
-  [0x61] = "?",
-  [0x70] = "?",
-  [0x77] = "? (Update Display)",
-  [0x78] = "?",
-  [0x81] = "?",
+--  [0x14] = "",
+--  [0x18] = "Message Text",
+--  [0x3c] = "",
+--  [0x3d] = "",
+--  [0x40] = "",
+--  [0x5a] = "",
+--  [0x5b] = "",
+--  [0x61] = "",
+--  [0x70] = "",
+--  [0x77] = "Update Display",
+--  [0x78] = "",
+--  [0x81] = "",
 }
 
 local LOGICAL_VALUE = {
@@ -172,7 +175,7 @@ local LOGICAL_VALUE = {
 }
 
 ali_fields.byte_count     = ProtoField.uint16("LOGOTD.BC",    "Byte Count",     base.DEC)
-ali_fields.function_code  = ProtoField.uint8 ("LOGOTD.CMD",   "Function Code",  base.HEX, FUNCTION_CODES)
+ali_fields.command_code   = ProtoField.uint8 ("LOGOTD.CMD",   "Command Code",  	base.HEX, COMMAND_CODES)
 ali_fields.data           = ProtoField.bytes ("LOGOTD.data",  "Data")
 ali_fields.bit0           = ProtoField.uint8 ("LOGOTD.bit0",  "b0",             base.DEC, LOGICAL_VALUE, 0x01)
 ali_fields.bit1           = ProtoField.uint8 ("LOGOTD.bit1",  "b1",             base.DEC, LOGICAL_VALUE, 0x02)
@@ -494,15 +497,202 @@ local packet_helper = nil
 -- this holds the plain "data" Dissector, in case we can't dissect it as LOGOTD
 local data = Dissector.get("data")
 
-lookup_function_code = {
+lookup_command_code = {
   [0x08] = {
     dissect = function(tvb, pinfo, tree)
-      return 0
+      if tvb:len() < PBUS_ALI_HDR_LEN then return 0 end
+      -- 01 + 00 + Byte Count (8bit value)
+      local number_of_bytes = tvb(2,1):uint()
+      tree:add(ali_fields.data, tvb(0,2))
+      tree:add(ali_fields.byte_count, tvb(2,1))
+      -- Command Code + ...
+      local cmd_code = tvb(3,1):uint()
+      tree:add(ali_fields.command_code, tvb(3,1))
+      -- If this telegram has no data, then we are done here
+      if number_of_bytes == 1 then return PBUS_ALI_HDR_LEN end
+
+      -- ... Data + ...
+      local pdu_length = PBUS_ALI_HDR_LEN - 1 + number_of_bytes
+      if tvb:len() < pdu_length then return 0 end
+
+      if not default_settings.subdissect then
+        tree:add(pg_fields.data, tvb(PBUS_ALI_HDR_LEN, pdu_length - PBUS_ALI_HDR_LEN))
+      else
+        local offset = PBUS_ALI_HDR_LEN
+        local datatree = tree:add(tvb(PBUS_ALI_HDR_LEN, pdu_length - PBUS_ALI_HDR_LEN), "Data bytes")
+
+        -- digital inputs
+        local number_of_bits = 24
+        local bytes_to_consume = 3
+        local bitcount = NumberOfSetBits(tvb(offset, bytes_to_consume):bytes())
+        local subtree = datatree:add(tvb(offset, bytes_to_consume), string.format("Digital inputs [Bitcount %d]", bitcount))
+        local bit = 1
+        while bytes_to_consume > 0 do
+          subtree:add(tvb(offset,1), string.format("I%02u-I%02u", bit+7, bit))
+          subtree:add(ali_fields.bit0, tvb(offset,1))
+          subtree:add(ali_fields.bit1, tvb(offset,1))
+          subtree:add(ali_fields.bit2, tvb(offset,1))
+          subtree:add(ali_fields.bit3, tvb(offset,1))
+          subtree:add(ali_fields.bit4, tvb(offset,1))
+          subtree:add(ali_fields.bit5, tvb(offset,1))
+          subtree:add(ali_fields.bit6, tvb(offset,1))
+          subtree:add(ali_fields.bit7, tvb(offset,1))
+          bit = bit + 8
+          offset = offset + 1
+          bytes_to_consume = bytes_to_consume - 1
+        end
+
+        -- function keys
+        bitcount = NumberOfSetBits(tvb(offset,1):bytes(), 4)
+        subtree = datatree:add(tvb(offset,1), string.format("Function keys [Bitcount %d]", bitcount))
+        subtree:add(tvb(offset,1), "F04-F01")
+        subtree:add(ali_fields.bit0, tvb(offset,1))
+        subtree:add(ali_fields.bit1, tvb(offset,1))
+        subtree:add(ali_fields.bit2, tvb(offset,1))
+        subtree:add(ali_fields.bit3, tvb(offset,1))
+        offset = offset + 1
+  
+        -- digital outputs
+        number_of_bits = 16
+        bytes_to_consume = 2
+        bitcount = NumberOfSetBits(tvb(offset, bytes_to_consume):bytes())
+        subtree = datatree:add(tvb(offset, bytes_to_consume), string.format("Digital outputs [Bitcount %d]", bitcount))
+        bit = 1
+        while bytes_to_consume > 0 do
+          subtree:add(tvb(offset,1), string.format("Q%02u-Q%02u", bit+7, bit))
+          subtree:add(ali_fields.bit0, tvb(offset,1))
+          subtree:add(ali_fields.bit1, tvb(offset,1))
+          subtree:add(ali_fields.bit2, tvb(offset,1))
+          subtree:add(ali_fields.bit3, tvb(offset,1))
+          subtree:add(ali_fields.bit4, tvb(offset,1))
+          subtree:add(ali_fields.bit5, tvb(offset,1))
+          subtree:add(ali_fields.bit6, tvb(offset,1))
+          subtree:add(ali_fields.bit7, tvb(offset,1))
+          bit = bit + 8
+          offset = offset + 1
+          bytes_to_consume = bytes_to_consume - 1
+        end
+        
+        -- digital merkers
+        number_of_bits = 27
+        bytes_to_consume = 4
+        bitcount = NumberOfSetBits(tvb(offset, bytes_to_consume):bytes(), number_of_bits)
+        subtree = datatree:add(tvb(offset, bytes_to_consume), string.format("Digital merkers [Bitcount %d]", bitcount))
+        bit = 1
+        while bytes_to_consume > 0 do
+          subtree:add(tvb(offset,1), string.format("M%02u-M%02u", bit+7 > number_of_bits and number_of_bits or bit+7, bit))
+          subtree:add(ali_fields.bit0, tvb(offset,1))
+          subtree:add(ali_fields.bit1, tvb(offset,1))
+          subtree:add(ali_fields.bit2, tvb(offset,1))
+          bit = bit + 3
+          if bit < number_of_bits then
+            subtree:add(ali_fields.bit3, tvb(offset,1))
+            subtree:add(ali_fields.bit4, tvb(offset,1))
+            subtree:add(ali_fields.bit5, tvb(offset,1))
+            subtree:add(ali_fields.bit6, tvb(offset,1))
+            subtree:add(ali_fields.bit7, tvb(offset,1))
+            bit = bit + 5
+          end
+          offset = offset + 1
+          bytes_to_consume = bytes_to_consume - 1
+        end
+        
+        -- shift register
+    		bitcount = NumberOfSetBits(tvb(offset,1):bytes())
+        subtree = datatree:add(tvb(offset,1), string.format("Shift register [Bitcount %d]", bitcount))
+        subtree:add(tvb(offset,1), "S08-S01")
+        subtree:add(ali_fields.bit0, tvb(offset,1))
+        subtree:add(ali_fields.bit1, tvb(offset,1))
+        subtree:add(ali_fields.bit2, tvb(offset,1))
+        subtree:add(ali_fields.bit3, tvb(offset,1))
+        subtree:add(ali_fields.bit4, tvb(offset,1))
+        subtree:add(ali_fields.bit5, tvb(offset,1))
+        subtree:add(ali_fields.bit6, tvb(offset,1))
+        subtree:add(ali_fields.bit7, tvb(offset,1))
+        offset = offset + 1
+        
+        -- cursor keys
+    		bitcount = NumberOfSetBits(tvb(offset,1):bytes(), 4)
+        subtree = datatree:add(tvb(offset,1), string.format("Cursor keys [Bitcount %d]", bitcount))
+        subtree:add(tvb(offset,1), "C04-C01")
+        subtree:add(ali_fields.bit0, tvb(offset,1))
+        subtree:add(ali_fields.bit1, tvb(offset,1))
+        subtree:add(ali_fields.bit2, tvb(offset,1))
+        subtree:add(ali_fields.bit3, tvb(offset,1))
+        offset = offset + 1
+  
+        -- analog inputs (16bit Big Endian)
+        bytes_to_consume = 16
+        local flag = 0
+        for i = offset, offset+bytes_to_consume-1 do
+          if tvb(i,1):uint() ~= 0 then
+          flag = 1
+          break
+          end
+        end
+        subtree = datatree:add(tvb(offset,bytes_to_consume), string.format("Analog inputs [%s]", FLAG_VALUE[flag]))
+        local analog = 1
+        while bytes_to_consume > 0 do
+          subtree:add(tvb(offset,2), string.format("AI%u: %d", analog, tvb(offset,2):int()))
+          analog = analog + 1
+          offset = offset + 2
+          bytes_to_consume = bytes_to_consume - 2
+        end
+  
+        -- analog outputs (16bit Big Endian)
+        flag = 0
+        for i = offset, offset+4-1 do
+          if tvb(i,1):uint() ~= 0 then
+          flag = 1
+          break
+          end
+        end
+        subtree = datatree:add(tvb(offset,4), string.format("Analog outputs [%s]", FLAG_VALUE[flag]))
+        subtree:add(tvb(offset,2), string.format("AQ1: %d", tvb(offset,2):int()))
+        offset = offset + 2
+        subtree:add(tvb(offset,2), string.format("AQ2: %d", tvb(offset,2):int()))
+        offset = offset + 2
+  
+        -- analog merkers (16bit Big Endian)
+        bytes_to_consume = 12
+        flag = 0
+        for i = offset, offset+bytes_to_consume-1 do
+          if tvb(i,1):uint() ~= 0 then
+          flag = 1
+          break
+          end
+        end
+        subtree = datatree:add(tvb(offset,bytes_to_consume), string.format("Analog merkers [%s]", FLAG_VALUE[flag]))
+        local analog = 1
+        while bytes_to_consume > 0 do
+          subtree:add(tvb(offset,2), string.format("AM%u: %d", analog, tvb(offset,2):int()))
+          analog = analog + 1
+          offset = offset + 2
+          bytes_to_consume = bytes_to_consume - 2
+        end
+      end
+
+      return pdu_length
     end
   },
   [0x09] = {
     dissect = function(tvb, pinfo, tree)
-      return 0
+      if tvb:len() < PBUS_ALI_HDR_LEN then return 0 end
+      -- 01 + 00 + Byte Count (8bit value)
+      local number_of_bytes = tvb(2,1):uint()
+      tree:add(ali_fields.data, tvb(0,2))
+      tree:add(ali_fields.byte_count, tvb(2,1))
+      -- Command Code + ...
+      local cmd_code = tvb(3,1):uint()
+      tree:add(ali_fields.command_code, tvb(3,1))
+      -- If this telegram has no data, then we are done here
+      if number_of_bytes == 1 then return PBUS_ALI_HDR_LEN end
+
+      -- ... Data + ...
+      local pdu_length = PBUS_ALI_HDR_LEN - 1 + number_of_bytes
+      if tvb:len() < pdu_length then return 0 end
+      tree:add(ali_fields.data, tvb(PBUS_ALI_HDR_LEN, pdu_length - PBUS_ALI_HDR_LEN))
+      return pdu_length
     end
   },
 }
@@ -665,24 +855,30 @@ function LOGOTD.dissector(tvb, pinfo, tree)
   -- set the protocol column to show our protocol name
   pinfo.cols.protocol = LOGOTD.name
 
-  -- append the INFO column
-  if string.find(tostring(pinfo.cols.info), "ALI") == nil then
-    pinfo.cols.info:append(string.format(", ALI length %d", ali_length))
-  end
-  
   -- add our protocol to the dissection display tree.
   local subtree = tree:add(LOGOTD)
+  local offset = 0
+  local cmd_code = tvb(3,1):uint()
+  if lookup_command_code[cmd_code] then
+    offset = lookup_command_code[cmd_code].dissect(tvb, pinfo, subtree)
+  else
+    -- 01 + 00 + Byte Count (8bit value) + Command Code + ...
+    subtree:add(ali_fields.data, tvb(0,2))
+    subtree:add(ali_fields.byte_count, tvb(2,1))
+    subtree:add(ali_fields.command_code, tvb(3,1))
+    offset = PBUS_ALI_HDR_LEN
+  end
 
-  local offset = 2
-  subtree:add(ali_fields.byte_count, tvb(offset, 1))
-  offset = offset + 1
-  subtree:add(ali_fields.function_code, tvb(offset, 1))
-  offset = offset + 1
+  -- append the INFO column
+  if string.find(tostring(pinfo.cols.info), " Len=") == nil then
+    if COMMAND_CODES[cmd_code] then
+      pinfo.cols.info:append(string.format(", %s", COMMAND_CODES[cmd_code]))
+    else
+      pinfo.cols.info:append(string.format(", CMD=0x%02x", cmd_code))
+    end
+    pinfo.cols.info:append(string.format(" Len=%d", ali_length))
+  end
   
---  if lookup_function_code[cmd] ~= nil then
---    offset = lookup_function_code[cmd].dissect(tvb, pinfo, subtree)
---  end
-
   -- if we got here, then we have only data bytes in the Tvb buffer
   data:call(tvb(offset, ali_length - offset):tvb(), pinfo, subtree)
 
@@ -736,14 +932,8 @@ local function dissectPBUS(tvb, pinfo, tree, offset)
   -- if we got here, then we have a SD2 telegram in the Tvb buffer
   -- so let's finish dissecting it...
 
-  -- set the INFO column, but only if we haven't already set it before for this
-  -- packet, because this function can be called multiple times per packet
-  local telegram = tvb(offset, 1):uint()
-  if string.find(tostring(pinfo.cols.info), "(0x%d%d)") == nil and FDL_CODES[telegram] ~= nil then
-    pinfo.cols.info:set(string.format("%s (0x%02x)", FDL_CODES[telegram], telegram))
-  end
-
   -- dissect the header (SD2) incl. there length fields (LE, LEr)
+  local telegram = tvb(offset, 1):uint()
   tree:add(fdl_fields.telegram, tvb(offset, 1))
   offset = offset + 1
   tree:add(fdl_fields.le, tvb(offset, 2))
@@ -765,14 +955,17 @@ local function dissectPBUS(tvb, pinfo, tree, offset)
   offset = offset + 1
 
   -- set the SOURCE and DESITNATION columns
-  pinfo.cols.src:set(string.format("0x%02x (%s)", src_addr, ADDRESS_VALUE[src_addr]))
-  pinfo.cols.dst:set(string.format("0x%02x (%s)", dst_addr, ADDRESS_VALUE[dst_addr]))
+  if string.find(tostring(pinfo.cols.src), "0x%x%x") == nil then
+    pinfo.cols.src:set(string.format("0x%02x", src_addr))
+    pinfo.cols.dst:set(string.format("0x%02x", dst_addr))
+  end
 
   -- dissect the "frame control" field (FC)
+  local frame_control = tvb(offset, 1):uint()
   tree:add(fdl_fields.fc, tvb(offset, 1))
   offset = offset + 1
 
-  -- dissect the "data unit" fields (DSAP and SSAP)
+ -- dissect the "data unit" fields (DSAP and SSAP)
   local subtree = tree:add(tvb(offset, length - PBUS_SD2_HDR_LEN - PBUS_TRAILER_LEN), "Data Unit (DU)")
   subtree:add(fdl_fields.dsap, tvb(offset, 1))
   subtree:add(fdl_fields.ssap, tvb(offset + 1, 1))
@@ -788,6 +981,29 @@ local function dissectPBUS(tvb, pinfo, tree, offset)
   end
   offset = offset + 1
   tree:add(fdl_fields.ed, tvb(offset, 1))
+
+  -- set the INFO column, but only if we haven't already set it before for this
+  -- packet, because this function can be called multiple times per packet
+  if string.find(tostring(pinfo.cols.info), "Seq=%d") == nil then
+    local src = string.sub(ADDRESS_VALUE[src_addr], 1, 1)
+    local dst = string.sub(ADDRESS_VALUE[dst_addr], 1, 1)
+    if src and dst then
+      pinfo.cols.info:set(string.format("%s > %s", src, dst))
+    else
+      pinfo.cols.info:set(string.format("%d > %d", src_addr, dst_addr))
+    end
+    if FDL_CODES[telegram] then
+      pinfo.cols.info:append(string.format(" [%s", FDL_CODES[telegram]))
+    else
+      pinfo.cols.info:append(string.format(" [FDL=%0x%02x", telegram))
+    end
+    if FC_CODES[frame_control] then
+      pinfo.cols.info:append(string.format(", %s]", FC_CODES[frame_control]))
+    else
+      pinfo.cols.info:append(string.format(", FC=%0x%02x]", frame_control))
+    end
+    pinfo.cols.info:append(string.format(" Seq=%d", packet_helper:get_number()))
+  end
 
   -- we are finished, now the dissected length will be returned
   return length
@@ -836,7 +1052,7 @@ function PROFIBUS.dissector(tvb, pinfo, root)
   if packet_helper:fragmented() and packet_helper:more_fragment(pinfo) then
     -- From here we know that the frame is part of a PROFINET telegram (but not
     -- the last on).
-    pinfo.cols.info:set("Telegram Fragment")
+    pinfo.cols.info:set(string.format("[Telegram Fragment] Seq=%d", packet_helper:get_number()))
     -- The Tvb buffer is a fragment of a PROFINET telegram, so display only the
     -- meta fields and the data to the tree
     local subtree = root:add(PROFIBUS)
